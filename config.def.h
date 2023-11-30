c296933e1207e7c2dc78676287f5bf5a47bf47bc
Mitch Goddard <mitch@mitchdotnet.net>
2023-11-29 22:59:42 -0500

    Font size and border tweaks for consistent terminal sizing

--- config.def.h
+++ config.def.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrainsMono NF:pixelsize=14:antialias=true:autohint=true";
-static int borderpx = 0;
+static char *font = "JetBrainsMono NF:pixelsize=15:antialias=true:autohint=true";
+static int borderpx = 1;
 
 /*
  * What program is execed by st depends of these precedence rules:
